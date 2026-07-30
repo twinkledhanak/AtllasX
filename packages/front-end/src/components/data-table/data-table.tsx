a47410cb4c173bac3daf633e5aaaaa17a93996e2
DhanakTwinkle <twinkledhanak@gmail.com>
2026-07-30 16:34:05 -0700

    UI: Handling column Visibility

--- packages/front-end/src/components/data-table/data-table.tsx
+++ packages/front-end/src/components/data-table/data-table.tsx
@@ -8,7 +8,7 @@ import {
   SortingState,
 } from "@tanstack/react-table"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import {
   Table,
@@ -19,20 +19,35 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
+import { visibleColumns } from "./columns"
+
 export function DataTable({ columns, data }) {
   const [sorting, setSorting] = useState<SortingState>([])
+  const [columnVisibility, setColumnVisibility] = useState({})
 
   const table = useReactTable({
     data,
     columns,
-    state: { sorting },
+    state: { sorting, columnVisibility, },
     onSortingChange: setSorting,
+    onColumnVisibilityChange: setColumnVisibility,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
     manualSorting: true, // enables server-side sorting later
     enableColumnResizing: true,
     columnResizeMode: "onChange",
   })
+  useEffect(() => {
+    const visibilityState = {}
+  
+    table.getAllLeafColumns().forEach(col => {
+      visibilityState[col.id] = visibleColumns.includes(col.id)
+    })
+  
+    setColumnVisibility(visibilityState)
+  }, [table])
+  
+  
 
   return (
     <div className="rounded-lg border bg-card shadow-sm overflow-x-auto">
