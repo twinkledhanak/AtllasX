1abfbf4cc54f5587b40f5d727fb38bee4e572b68
DhanakTwinkle <twinkledhanak@gmail.com>
2026-08-01 14:48:29 -0700

    UI: Added all fields in alert dialog

--- packages/front-end/src/components/forms/UserForm.tsx
+++ packages/front-end/src/components/forms/UserForm.tsx
@@ -12,6 +12,7 @@ import {
 } from "@/components/ui/alert-dialog"
 
 import { useState, useEffect } from "react"
+import {UserBaseRequest} from "@/validations/user.request"
 
 export default function UserForm({
   open,
@@ -19,22 +20,53 @@ export default function UserForm({
   initialData = null,
   onSubmit,
 }) {
-  const [name, setName] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [middleName, setMiddleName] = useState("")
+  const [lastName, setLastName] = useState("")
   const [email, setEmail] = useState("")
+  const [phoneNumber, setPhoneNumber] = useState("")
+  const [address, setAddress] = useState("")
+  const [notes, setNotes] = useState("")
+  const [errors, setErrors] = useState<Record<string, string[]>>({})
 
   // Pre-fill fields when editing
   useEffect(() => {
     if (initialData) {
-      setName(initialData.name || "")
+      console.log(initialData)
+      setFirstName(initialData.firstName || "")
+      setMiddleName(initialData.middleName || "")
+      setLastName(initialData.lastName || "")
       setEmail(initialData.email || "")
+      setPhoneNumber(initialData.phoneNumber || "")
+      setAddress(initialData.address || "")
+      setNotes(initialData.notes || "")
     }
   }, [initialData])
 
   const handleSubmit = () => {
-    onSubmit({ name, email })
+    const result = UserBaseRequest.safeParse({
+      firstName,
+      middleName: middleName || '',
+      lastName,
+      email,
+      phoneNumber: phoneNumber || '',
+      address: address || '',
+      notes: notes || '',
+      registeredAt: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+  
+    if (!result.success) {
+      setErrors(result.error.flatten().fieldErrors)
+      return
+    }
+  
+    // If valid → submit to parent
+    onSubmit(result.data)
     onOpenChange(false)
   }
-
+  
   return (
     <AlertDialog open={open} onOpenChange={onOpenChange}>
       <AlertDialogContent>
@@ -52,19 +84,81 @@ export default function UserForm({
         <div className="space-y-4 mt-4">
           <input
             type="text"
-            placeholder="Name"
+            placeholder="Full Name *"
             className="w-full border rounded p-2"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
           />
+          {errors.firstName && (
+          <p className="text-red-600 text-sm">{errors.firstName[0]}</p>
+          )}
+
+          <input
+            type="text"
+            placeholder="Middle Name"
+            className="w-full border rounded p-2"
+            value={middleName}
+            onChange={(e) => setMiddleName(e.target.value)}
+          />
+          {errors.middlename && (
+          <p className="text-red-600 text-sm">{errors.middleName[0]}</p>
+          )}
+
+          <input
+            type="text"
+            placeholder="Last Name *"
+            className="w-full border rounded p-2"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          {errors.lastName && (
+          <p className="text-red-600 text-sm">{errors.lastName[0]}</p>
+          )}
 
           <input
             type="email"
-            placeholder="Email"
+            placeholder="Email *"
             className="w-full border rounded p-2"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          {errors.email && (
+          <p className="text-red-600 text-sm">{errors.email[0]}</p>
+          )}
+
+          <input
+            type="text"
+            placeholder="Phone Number"
+            className="w-full border rounded p-2"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+          {errors.phoneNumber && (
+          <p className="text-red-600 text-sm">{errors.phoneNumber[0]}</p>
+          )}
+
+          <input
+            type="text"
+            placeholder="Address"
+            className="w-full border rounded p-2"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          {errors.address && (
+          <p className="text-red-600 text-sm">{errors.address[0]}</p>
+          )}
+
+          <input
+            type="text"
+            placeholder="Admin Notes"
+            className="w-full border rounded p-2"
+            value={notes}
+            onChange={(e) => setNotes(e.target.value)}
+          />
+          {errors.notes && (
+          <p className="text-red-600 text-sm">{errors.notes[0]}</p>
+          )}
+
         </div>
 
         <AlertDialogFooter>
