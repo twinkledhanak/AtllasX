081a1d38ae728141bd9f4047a3e2d345afe1f672
DhanakTwinkle <twinkledhanak@gmail.com>
2026-08-01 18:42:33 -0700

    UI: Reformating the file

--- packages/front-end/src/validations/user.request.ts
+++ packages/front-end/src/validations/user.request.ts
@@ -1,14 +1,13 @@
 import { z } from "zod"
 
 export const UserBaseRequest = z.object({
-  fullName: z.string().min(1, "Full Name is required").trim(),
+  firstName: z.string().min(1, "First Name is required").trim(),
   middleName: z.string().optional(),
   lastName: z.string().min(1, "Last Name is required").trim(),
   email: z.email("Invalid email format").trim(),
   phoneNumber: z.string().optional(),
   address: z.string().optional(),
   notes: z.string().optional(),
-  registeredAt: z.coerce.date(),
-  createdAt: z.coerce.date(),
-  updatedAt: z.coerce.date(),
+  // Removing all Date() fields here, Express to handle all timestamps.
+  // Different client browsers can have different timestamps and zones.
 })
