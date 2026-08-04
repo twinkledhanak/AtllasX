9b563828de47f976d56969d1e9e3787faefe74df
DhanakTwinkle <twinkledhanak@gmail.com>
2026-08-03 21:28:50 -0700

    UI: Added sort support for default column + direction

--- packages/front-end/src/hooks/populateUsers.tsx
+++ packages/front-end/src/hooks/populateUsers.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 
-export function populateUsers({ page, pageSize, search }) {
+export function populateUsers({ page, pageSize, search, sorting}) {
   const [data, setData] = useState([]);
   const [total, setTotal] = useState(0);
   const [loading, setLoading] = useState(true);
+
+  const sort = sorting[0]?.id || "firstName";
+  const direction = sorting[0]?.desc ? "desc" : "asc";
+
 
   useEffect(() => {
     async function load() {
@@ -11,7 +15,7 @@ export function populateUsers({ page, pageSize, search }) {
 
       try {
         const res = await fetch(
-          `http://localhost:50000/users?page=${page}&pageSize=${pageSize}&search=${encodeURIComponent(search || "")}`
+          `http://localhost:50000/users?page=${page}&pageSize=${pageSize}&search=${encodeURIComponent(search || "")}&sort=${sort}&direction=${direction}`
         );
 
         const json = await res.json();
@@ -24,7 +28,7 @@ export function populateUsers({ page, pageSize, search }) {
     }
 
     load();
-  }, [page, pageSize, search]);
+  }, [page, pageSize, search, sorting]); // Fix for desc button toggle not working
 
   return { data, total, loading };
 }
